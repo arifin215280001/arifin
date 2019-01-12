0b52174b1e2df84c3fd131c999bded88908e20b2
arifin215280001 <46618498+arifin215280001@users.noreply.github.com>
2019-01-11 22:32:22 -0800

    Create arifin.php

---
+++ arifin.php
@@ -0,0 +1,19 @@
+<?php
+$nim = "21528000";
+$nama = "Mohamad Arifin";
+$kelas = "A";
+
+echo "<table>
+        <tr>
+          <td>NIM</td>
+          <td>:$nim</td>
+         </tr>
+         <tr>
+          <td>NAMA</td>
+          <td>:$nama</td>
+         </tr>
+         <tr>
+          <td>KELAS</td>
+          <td>:$kelas</td>
+         </tr>
+        </table>;
